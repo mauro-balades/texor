e2e89f7085f2418551490c95c3426b93937b30b3
maubg-debug <maubg@tutanota.com>
2021-09-01 11:44:22 +0200

    added: prettier code formatting, sintax highlighting, writing, status bar

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-#![warn(clippy::all, clippy::pedantic)]
-mod editor;
-mod terminal;
+#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::shadow_reuse,
+    clippy::print_stdout,
+    clippy::wildcard_enum_match_arm,
+    clippy::else_if_without_else
+)]
 mod document;
+mod editor;
+mod filetype;
+mod highlighting;
 mod row;
-
-pub use row::Row;
+mod terminal;
 pub use document::Document;
 use editor::Editor;
-pub use terminal::Terminal;
 pub use editor::Position;
+pub use editor::SearchDirection;
+pub use filetype::FileType;
+pub use filetype::HighlightingOptions;
+pub use row::Row;
+pub use terminal::Terminal;
 
 fn main() {
     Editor::default().run();
---
+++ test/sintax.py
@@ -0,0 +1,11 @@
+
+num = 0
+string = ""
+
+def main():
+    for i in range(0):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
